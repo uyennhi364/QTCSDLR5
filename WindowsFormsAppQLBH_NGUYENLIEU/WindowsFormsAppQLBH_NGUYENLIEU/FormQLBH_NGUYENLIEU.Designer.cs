85276b1f6ad8cd891a464f03d8d51f0fcfdde45f
uyennhi364 <leuyennhi030604@gmail.com>
2025-05-21 01:11:34 +0700

    Add files via upload

---
+++ WindowsFormsAppQLBH_NGUYENLIEU/WindowsFormsAppQLBH_NGUYENLIEU/FormQLBH_NGUYENLIEU.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace WindowsFormsAppQLBH_NGUYENLIEU
+{
+    partial class FormQLBH_NGUYENLIEU
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_MANL = new System.Windows.Forms.Label();
+            this.txt_MANL = new System.Windows.Forms.TextBox();
+            this.lbl_TENNL = new System.Windows.Forms.Label();
+            this.txt_TENNL = new System.Windows.Forms.TextBox();
+            this.lbl_DONVI = new System.Windows.Forms.Label();
+            this.txt_DONVI = new System.Windows.Forms.TextBox();
+            this.lbl_SLTONKHO = new System.Windows.Forms.Label();
+            this.txt_SLTONKHO = new System.Windows.Forms.TextBox();
+            this.lbl_BANGTEN = new System.Windows.Forms.Label();
+            this.btn_Them = new System.Windows.Forms.Button();
+            this.btn_Sua = new System.Windows.Forms.Button();
+            this.btn_Xoa = new System.Windows.Forms.Button();
+            this.DataGridViewNGUYENLIEU = new System.Windows.Forms.DataGridView();
+            this.btn_TimKiem = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridViewNGUYENLIEU)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_MANL
+            // 
+            this.lbl_MANL.AutoSize = true;
+            this.lbl_MANL.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_MANL.Location = new System.Drawing.Point(44, 100);
+            this.lbl_MANL.Name = "lbl_MANL";
+            this.lbl_MANL.Size = new System.Drawing.Size(135, 21);
+            this.lbl_MANL.TabIndex = 0;
+            this.lbl_MANL.Text = "Mã Nguyên Liệu";
+            // 
+            // txt_MANL
+            // 
+            this.txt_MANL.Location = new System.Drawing.Point(219, 103);
+            this.txt_MANL.Name = "txt_MANL";
+            this.txt_MANL.Size = new System.Drawing.Size(100, 20);
+            this.txt_MANL.TabIndex = 0;
+            // 
+            // lbl_TENNL
+            // 
+            this.lbl_TENNL.AutoSize = true;
+            this.lbl_TENNL.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_TENNL.Location = new System.Drawing.Point(44, 163);
+            this.lbl_TENNL.Name = "lbl_TENNL";
+            this.lbl_TENNL.Size = new System.Drawing.Size(138, 21);
+            this.lbl_TENNL.TabIndex = 0;
+            this.lbl_TENNL.Text = "Tên Nguyên Liệu";
+            // 
+            // txt_TENNL
+            // 
+            this.txt_TENNL.Location = new System.Drawing.Point(219, 163);
+            this.txt_TENNL.Name = "txt_TENNL";
+            this.txt_TENNL.Size = new System.Drawing.Size(100, 20);
+            this.txt_TENNL.TabIndex = 1;
+            // 
+            // lbl_DONVI
+            // 
+            this.lbl_DONVI.AutoSize = true;
+            this.lbl_DONVI.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_DONVI.Location = new System.Drawing.Point(44, 227);
+            this.lbl_DONVI.Name = "lbl_DONVI";
+            this.lbl_DONVI.Size = new System.Drawing.Size(62, 21);
+            this.lbl_DONVI.TabIndex = 0;
+            this.lbl_DONVI.Text = "Đơn Vị";
+            // 
+            // txt_DONVI
+            // 
+            this.txt_DONVI.Location = new System.Drawing.Point(219, 227);
+            this.txt_DONVI.Name = "txt_DONVI";
+            this.txt_DONVI.Size = new System.Drawing.Size(100, 20);
+            this.txt_DONVI.TabIndex = 2;
+            // 
+            // lbl_SLTONKHO
+            // 
+            this.lbl_SLTONKHO.AutoSize = true;
+            this.lbl_SLTONKHO.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_SLTONKHO.Location = new System.Drawing.Point(44, 285);
+            this.lbl_SLTONKHO.Name = "lbl_SLTONKHO";
+            this.lbl_SLTONKHO.Size = new System.Drawing.Size(146, 21);
+            this.lbl_SLTONKHO.TabIndex = 0;
+            this.lbl_SLTONKHO.Text = "Số lượng Tồn Kho";
+            // 
+            // txt_SLTONKHO
+            // 
+            this.txt_SLTONKHO.Location = new System.Drawing.Point(219, 285);
+            this.txt_SLTONKHO.Name = "txt_SLTONKHO";
+            this.txt_SLTONKHO.Size = new System.Drawing.Size(100, 20);
+            this.txt_SLTONKHO.TabIndex = 3;
+            // 
+            // lbl_BANGTEN
+            // 
+            this.lbl_BANGTEN.AutoSize = true;
+            this.lbl_BANGTEN.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_BANGTEN.Location = new System.Drawing.Point(290, 31);
+            this.lbl_BANGTEN.Name = "lbl_BANGTEN";
+            this.lbl_BANGTEN.Size = new System.Drawing.Size(224, 25);
+            this.lbl_BANGTEN.TabIndex = 0;
+            this.lbl_BANGTEN.Text = "QUẢN LÝ NGUYÊN LIỆU";
+            // 
+            // btn_Them
+            // 
+            this.btn_Them.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Them.Location = new System.Drawing.Point(48, 363);
+            this.btn_Them.Name = "btn_Them";
+            this.btn_Them.Size = new System.Drawing.Size(75, 27);
+            this.btn_Them.TabIndex = 4;
+            this.btn_Them.Text = "Thêm";
+            this.btn_Them.UseVisualStyleBackColor = true;
+            // 
+            // btn_Sua
+            // 
+            this.btn_Sua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Sua.Location = new System.Drawing.Point(160, 363);
+            this.btn_Sua.Name = "btn_Sua";
+            this.btn_Sua.Size = new System.Drawing.Size(75, 27);
+            this.btn_Sua.TabIndex = 5;
+            this.btn_Sua.Text = "Sửa";
+            this.btn_Sua.UseVisualStyleBackColor = true;
+            // 
+            // btn_Xoa
+            // 
+            this.btn_Xoa.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Xoa.Location = new System.Drawing.Point(274, 363);
+            this.btn_Xoa.Name = "btn_Xoa";
+            this.btn_Xoa.Size = new System.Drawing.Size(75, 27);
+            this.btn_Xoa.TabIndex = 6;
+            this.btn_Xoa.Text = "Xóa";
+            this.btn_Xoa.UseVisualStyleBackColor = true;
+            // 
+            // DataGridViewNGUYENLIEU
+            // 
+            this.DataGridViewNGUYENLIEU.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridViewNGUYENLIEU.Location = new System.Drawing.Point(374, 85);
+            this.DataGridViewNGUYENLIEU.Name = "DataGridViewNGUYENLIEU";
+            this.DataGridViewNGUYENLIEU.Size = new System.Drawing.Size(401, 353);
+            this.DataGridViewNGUYENLIEU.TabIndex = 0;
+            this.DataGridViewNGUYENLIEU.TabStop = false;
+            // 
+            // btn_TimKiem
+            // 
+            this.btn_TimKiem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TimKiem.Location = new System.Drawing.Point(150, 406);
+            this.btn_TimKiem.Name = "btn_TimKiem";
+            this.btn_TimKiem.Size = new System.Drawing.Size(85, 32);
+            this.btn_TimKiem.TabIndex = 7;
+            this.btn_TimKiem.Text = "Tìm Kiếm ";
+            this.btn_TimKiem.UseVisualStyleBackColor = true;
+            // 
+            // FormQLBH_NGUYENLIEU
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_TimKiem);
+            this.Controls.Add(this.DataGridViewNGUYENLIEU);
+            this.Controls.Add(this.btn_Xoa);
+            this.Controls.Add(this.btn_Sua);
+            this.Controls.Add(this.btn_Them);
+            this.Controls.Add(this.lbl_BANGTEN);
+            this.Controls.Add(this.txt_SLTONKHO);
+            this.Controls.Add(this.lbl_SLTONKHO);
+            this.Controls.Add(this.txt_DONVI);
+            this.Controls.Add(this.lbl_DONVI);
+            this.Controls.Add(this.txt_TENNL);
+            this.Controls.Add(this.lbl_TENNL);
+            this.Controls.Add(this.txt_MANL);
+            this.Controls.Add(this.lbl_MANL);
+            this.Name = "FormQLBH_NGUYENLIEU";
+            this.Text = "QLBH_NGUYENLIEU";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridViewNGUYENLIEU)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_MANL;
+        private System.Windows.Forms.TextBox txt_MANL;
+        private System.Windows.Forms.Label lbl_TENNL;
+        private System.Windows.Forms.TextBox txt_TENNL;
+        private System.Windows.Forms.Label lbl_DONVI;
+        private System.Windows.Forms.TextBox txt_DONVI;
+        private System.Windows.Forms.Label lbl_SLTONKHO;
+        private System.Windows.Forms.TextBox txt_SLTONKHO;
+        private System.Windows.Forms.Label lbl_BANGTEN;
+        private System.Windows.Forms.Button btn_Them;
+        private System.Windows.Forms.Button btn_Sua;
+        private System.Windows.Forms.Button btn_Xoa;
+        private System.Windows.Forms.DataGridView DataGridViewNGUYENLIEU;
+        private System.Windows.Forms.Button btn_TimKiem;
+    }
+}
+
+
---
+++ WindowsFormsAppQLBH_NGUYENLIEU/WindowsFormsAppQLBH_NGUYENLIEU/FormQLBH_NGUYENLIEU.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppQLBH_NGUYENLIEU
+{
+    public partial class FormQLBH_NGUYENLIEU : Form
+    {
+        string connectionString = "Data Source=.;Initial Catalog=QLBH;Integrated Security=True";
+        public FormQLBH_NGUYENLIEU()
+        {
+            InitializeComponent();
+            this.Load += Form_Load;
+            this.FormClosing += Form_Closing;
+
+            DataGridViewNGUYENLIEU.CellClick += DataGridViewNGUYENLIEU_CellClick;
+            btn_Them.Click += btn_Them_Click;
+            btn_Sua.Click += btn_Sua_Click;
+            btn_Xoa.Click += btn_Xoa_Click;
+            btn_TimKiem.Click += btn_TimKiem_Click;
+
+
+
+        }
+        private void Form_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM NGUYENLIEU";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                DataGridViewNGUYENLIEU.DataSource = dt;
+            }
+        }
+
+        private void ClearInputs()
+        {
+            txt_MANL.Clear();
+            txt_TENNL.Clear();
+            txt_DONVI.Clear();
+            txt_SLTONKHO.Clear();
+            txt_MANL.Focus();
+        }
+
+        private bool IsValidMaNL(string ma)
+        {
+            return ma.Length == 10 && ma.StartsWith("NL") && ma.Substring(2).All(char.IsDigit);
+        }
+
+        private bool IsValidInput()
+        {
+            if (!IsValidMaNL(txt_MANL.Text))
+            {
+                MessageBox.Show("Mã nguyên liệu phải có dạng 'NL' + 8 chữ số. Ví dụ: NL00000001", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txt_TENNL.Text))
+            {
+                MessageBox.Show("Tên nguyên liệu không được để trống.");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txt_DONVI.Text))
+            {
+                MessageBox.Show("Đơn vị không được để trống.");
+                return false;
+            }
+
+            if (!int.TryParse(txt_SLTONKHO.Text, out int sl) || sl < 0)
+            {
+                MessageBox.Show("Số lượng tồn kho phải là số nguyên không âm.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btn_Them_Click(object sender, EventArgs e)
+        {
+            if (!IsValidInput()) return;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string checkQuery = "SELECT COUNT(*) FROM NGUYENLIEU WHERE MANL = @MANL";
+                SqlCommand checkCmd = new SqlCommand(checkQuery, conn);
+                checkCmd.Parameters.AddWithValue("@MANL", txt_MANL.Text);
+                conn.Open();
+
+                int exists = (int)checkCmd.ExecuteScalar();
+                if (exists > 0)
+                {
+                    MessageBox.Show("Mã nguyên liệu đã tồn tại.");
+                    conn.Close();
+                    return;
+                }
+
+                string query = "INSERT INTO NGUYENLIEU (MANL, TENNL, DONVI, SLTONKHO) VALUES (@MANL, @TENNL, @DONVI, @SLTONKHO)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MANL", txt_MANL.Text);
+                cmd.Parameters.AddWithValue("@TENNL", txt_TENNL.Text);
+                cmd.Parameters.AddWithValue("@DONVI", txt_DONVI.Text);
+                cmd.Parameters.AddWithValue("@SLTONKHO", int.Parse(txt_SLTONKHO.Text));
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Thêm thành công!");
+                    LoadData();
+                    ClearInputs();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi thêm: " + ex.Message);
+                }
+            }
+        }
+
+        private void btn_Sua_Click(object sender, EventArgs e)
+        {
+            if (!IsValidInput()) return;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE NGUYENLIEU SET TENNL = @TENNL, DONVI = @DONVI, SLTONKHO = @SLTONKHO WHERE MANL = @MANL";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MANL", txt_MANL.Text);
+                cmd.Parameters.AddWithValue("@TENNL", txt_TENNL.Text);
+                cmd.Parameters.AddWithValue("@DONVI", txt_DONVI.Text);
+                cmd.Parameters.AddWithValue("@SLTONKHO", int.Parse(txt_SLTONKHO.Text));
+
+                try
+                {
+                    conn.Open();
+                    int rows = cmd.ExecuteNonQuery();
+                    if (rows > 0)
+                        MessageBox.Show("Sửa thành công!");
+                    else
+                        MessageBox.Show("Không tìm thấy nguyên liệu để sửa.");
+
+                    LoadData();
+                    ClearInputs();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi sửa: " + ex.Message);
+                }
+            }
+        }
+
+        private void btn_Xoa_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM NGUYENLIEU WHERE MANL = @MANL";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MANL", txt_MANL.Text);
+
+                try
+                {
+                    conn.Open();
+                    int rows = cmd.ExecuteNonQuery();
+                    if (rows > 0)
+                        MessageBox.Show("Xóa thành công!");
+                    else
+                        MessageBox.Show("Không tìm thấy nguyên liệu để xóa.");
+
+                    LoadData();
+                    ClearInputs();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi xóa: " + ex.Message);
+                }
+            }
+        }
+
+        private void DataGridViewNGUYENLIEU_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = DataGridViewNGUYENLIEU.Rows[e.RowIndex];
+                txt_MANL.Text = row.Cells["MANL"].Value.ToString();
+                txt_TENNL.Text = row.Cells["TENNL"].Value.ToString();
+                txt_DONVI.Text = row.Cells["DONVI"].Value.ToString();
+                txt_SLTONKHO.Text = row.Cells["SLTONKHO"].Value.ToString();
+            }
+        }
+        private void btn_TimKiem_Click(object sender, EventArgs e)
+        {
+            string tuKhoa = txt_TENNL.Text.Trim();  // Tìm theo tên nguyên liệu
+
+            if (string.IsNullOrEmpty(tuKhoa))
+            {
+                LoadData();
+                return;
+            }
+
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string sQuery = "SELECT * FROM NGUYENLIEU WHERE TENNL LIKE @tuKhoa";
+                SqlCommand cmd = new SqlCommand(sQuery, con);
+                cmd.Parameters.AddWithValue("@tuKhoa", "%" + tuKhoa + "%");
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+
+                try
+                {
+                    con.Open();
+                    adapter.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        DataGridViewNGUYENLIEU.DataSource = dt;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không tìm thấy nguyên liệu nào phù hợp.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi tìm kiếm: " + ex.Message);
+                }
+            }
+        }
+
+
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Cảm ơn bạn đã sử dụng! Đóng ứng dụng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        
+       
+    }
+}
